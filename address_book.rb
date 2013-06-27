d038df27a831dde31c850d35b59afd5f2ebeaa34
Wesley Carr <surfswell17@gmail.com>
2013-06-27 16:56:56 -0400

    Entering data into contacts with ruby code (db.exec)

--- address_book.rb
+++ address_book.rb
@@ -4,15 +4,24 @@ require 'pry'
 puts "Hey gurl"
 
 #establishes connection to database (must be a hash)
-db = PG.connect(
-  :dbname => 'address_book',
-  :host => 'localhost'
-  )
-#passes in a string of sql & executes ( 'exec()' )
-# Dealing with the response from the database
-db.exec("select * from contacts") do |result|
-  result.each do |row|
-    binding.pry
-  end
-end
+# db = PG.connect(
+#   :dbname => 'address_book',
+#   :host => 'localhost')
+# #passes in a string of sql & executes ( 'exec()' )
+# # Reads from database
+# sql = "select first, last, age, gender, dtgd, phone"
+# db.exec(sql) do |result|
+#   result.each do |row|
+#     binding.pry
+#   end
+# end
 # ^ selects all(*) from contacts in address_book database
+# db.close
+
+db = PG.connect( :dbname => 'address_book', :host => 'localhost')
+
+sql = "insert into contacts (first, last, age, gender, dtgd, phone)
+  values ('ben', 'israeli', 26, 'm', 'true', '310-923-2319')"
+
+db.exec(sql)
+db.close
