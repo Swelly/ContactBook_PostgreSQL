85d95842736f46b25b839834ffdc5234b4b114c1
Wesley Carr <surfswell17@gmail.com>
2013-06-28 16:34:14 -0400

    Deleting and updating via contact IDs

--- main.rb
+++ main.rb
@@ -4,11 +4,27 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'rainbow'
 
+# helpers do
+#   def open_db
+#     PG.connect(:dbname => 'address_book', :host => 'localhost')
+#   end
+# end
+
+#run sql database
+def run_sql(sql)
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  result = db.exec(sql)
+  db.close
+  result
+end
+
+#Show index page
 get '/' do
   puts "This is the index"
   erb :index
 end
 
+# Retrieve contacts from database
 get '/contacts' do
   puts ": Database opened".color(:blue)
   db = PG.connect(:dbname => 'address_book', :host => 'localhost')
@@ -20,10 +36,12 @@ get '/contacts' do
   erb :contacts
 end
 
+#Go to form to create new contact
 get '/contact/new_contact' do
   erb :new_contact
 end
 
+#Post the new contact to database
 post '/contacts' do
 
   first = params[:first]
@@ -41,13 +59,46 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
-post '/contacts/delete' do
-  "Concrete shoes"
-  #Figure out who to delete
-  #Write sql to delete them
-  #Go someplace
+#Get contact to edit
+get '/contacts/:id/edit' do
+  id = params[:id]
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  sql = "SELECT * FROM contacts WHERE id = #{id}"
+  @contact = db.exec(sql).first
+  db.close
+  erb :edit
 end
 
+#Update specific id in database
+post '/contacts/:id' do
+
+  id = params[:id]
+  first = params[:first]
+  last = params[:last]
+  age = params[:age]
+  gender = params[:gender]
+  dtgt = params[:dtgt]
+  phone = params[:phone]
+
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+
+  #telling sql to update the specific ID(contact) with all changed form values
+  sql = "UPDATE contacts SET (first, last, age, gender, dtgt, phone) = ('#{first}', '#{last}', '#{age}', '#{gender}', '#{dtgt}', '#{phone}') WHERE id = #{id}"
+
+  db.exec(sql)
+  db.close
+  redirect to('/contacts')
+end
+
+post '/contacts/:id/delete' do
+  @id = params[:id]
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  sql = "DELETE FROM contacts WHERE id = #{@id}"
+  db.exec(sql)
+  db.close
+  redirect to "/contacts"
+end
+#Get contacts
 get '/contacts/:id' do
   @id = params[:id]
   db = PG.connect(:dbname => 'address_book', :host => 'localhost')
