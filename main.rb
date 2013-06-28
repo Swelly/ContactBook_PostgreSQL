a1c4a6e67f89419146df8c33cdb80ad7730123b9
Wesley Carr <surfswell17@gmail.com>
2013-06-28 11:23:50 -0400

    Posting new contact to database correctly. Cleaned up

--- main.rb
+++ main.rb
@@ -5,13 +5,18 @@ require 'sinatra/reloader' if development?
 require 'rainbow'
 
 get '/' do
+  puts "This is the index"
   erb :index
 end
 
 get '/contacts' do
+  puts ": Database opened".color(:blue)
   db = PG.connect(:dbname => 'address_book', :host => 'localhost')
   sql = "SELECT * FROM contacts"
   @contacts = db.exec(sql)
+  puts ":: @contacts pulled from database".color(:yellow)
+  db.close
+  puts "::: Database now closed".color(:magenta)
   erb :contacts
 end
 
@@ -19,8 +24,21 @@ get '/contact/new_contact' do
   erb :new_contact
 end
 
-post '/contact/new_contact' do
-  erb :new_contact
+post '/contacts' do
+
+  first = params[:first]
+  last = params[:last]
+  age = params[:age]
+  gender = params[:gender]
+  dtgd = params[:dtgd]
+  phone = params[:phone]
+
+  sql = "INSERT INTO CONTACTS (first, last, age, gender, dtgd, phone) VALUES ('#{first}', '#{last}', #{age}, '#{gender}', '#{dtgd}', '#{phone}')"
+
+  db = PG.connect(:dbname => 'address_book', :host => 'localhost')
+  db.exec(sql)
+  db.close
+  redirect to('/contacts')
 end
 
 get '/contacts/:name' do
